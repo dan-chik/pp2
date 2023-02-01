a0361ca8c37773c19995f52a6d0c57309f2d65ee
dan-chik <123585641+dan-chik@users.noreply.github.com>
2023-02-02 00:50:21 +0600

    operators

---
+++ operators/exercises.py
@@ -0,0 +1,18 @@
+# multiply
+print(10 * 5)
+
+# divide
+print(10 / 2)
+
+# membership operator "in" to check if "apple" is present in the fruits object
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+# comparison operator "> < == !=" to check if 5 is not equal to 10
+if 5 != 10:
+  print("5 and 10 is not equal")
+
+# logical operator "and or" to check if at least one of two statements is True
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true")
